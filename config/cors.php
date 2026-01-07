47a822f0c29e23347a4d37257c93aa96152d39b4
phil468 <jdelacruzcalderon@gmail.com>
2026-01-07 08:45:13 -0500

    feat: Actualizar configuración CORS para permitir orígenes específicos y habilitar credenciales

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,13 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:8100',
+        'http://localhost:8101', 
+        'http://localhost:8102',
+        'https://inspecciones-sst.vanguardfresh.pe',
+        'https://apps.vanguardfresh.pe',
+    ],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +35,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
