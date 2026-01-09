5c2c5f116cb1659ea01eee9805bb37f8f2b1b6a7
phil468 <jdelacruzcalderon@gmail.com>
2026-01-09 08:34:12 -0500

    feat: actualizar configuración del mailer para permitir cifrado opcional y desactivar verificación de pares

--- config/mail.php
+++ config/mail.php
@@ -39,12 +39,13 @@ return [
             // 'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
             'port' => env('MAIL_PORT', 587),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'encryption' => env('MAIL_ENCRYPTION') ?: null,
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             // 'local_domain' => env('MAIL_EHLO_DOMAIN'),
             'auth_mode' => null,
+            'verify_peer' => false,
             'stream' => [
                 'ssl' => [
                     'allow_self_signed' => true,
